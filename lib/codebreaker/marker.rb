7abe000831bcc8d612b4af979f6c5f8febe08447
Matt Wilbur <wilburm@gmail.com>
2011-08-21 15:42:08 -0400

    Added duplicate number marking scenario

--- lib/codebreaker/marker.rb
+++ lib/codebreaker/marker.rb
@@ -1,7 +1,7 @@
 class Marker
   def initialize(secret, guess)
-    @secret = secret
-    @guess = guess
+    @secret = secret.split('').freeze
+    @guess = guess.split('').freeze
   end
 
   def exact_match?(position)
@@ -20,9 +20,25 @@ class Marker
   end
 
   def number_match_count()
+    total_match_count - exact_match_count
+=begin
     (0..3).inject(0) do |count, index|
       count += 1 if number_match?(index)
       count
     end
+=end
   end
+
+  def total_match_count()
+    count = 0
+    scratch = @secret.dup
+    @guess.each do |n|
+      if scratch.include?(n)
+        scratch.delete_at(@secret.index(n))
+        count += 1
+      end
+    end
+    count
+  end
+
 end
